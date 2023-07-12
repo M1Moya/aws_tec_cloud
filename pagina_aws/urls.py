058814edb6b24dce433e547e607be01e3cc2e2fa
SakroSark <gustavo123_11@hotmail.com>
2023-07-12 01:38:23 -0400

    terminado todo
    soy lo mejor

--- pagina_aws/urls.py
+++ pagina_aws/urls.py
@@ -1,7 +1,10 @@
 
 from django.urls import path
+from .views import formCreateView,formCreateView2
 from . import views
 
 urlpatterns = [
-    path('', views.hello)
+    path('home/', views.hello,name="home"),
+    path("",formCreateView.as_view(), name="cuestionario" ) ,
+    path("cuestionario2/",formCreateView2.as_view(), name="cuestionario2" ) ,
 ]
--- pagina_aws/views.py
+++ pagina_aws/views.py
@@ -1,6 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
 # Create your views here.
 def hello(request):
     return render(request,"index.html")
+       
+class formCreateView(CreateView):
+    model = Dudas
+    template_name = "index.html"
+    fields = ["nombre", "apellido", "email", "dudas"]
+    success_url = reverse_lazy("home")
+    
+class formCreateView2(CreateView):
+    model = Suscripcion
+    template_name = "index.html"
+    fields = ["nombre", "apellido", "email"]
+    success_url = reverse_lazy("home")
